e2fd06009a5468f6e07adddd47ff1389cda4c0c8
Aganlumo <a.ga.ro271@gmail.com>
2021-02-23 11:28:49 -0600

    RepoSetup

--- readMultipleStreams.py
+++ readMultipleStreams.py
@@ -8,6 +8,7 @@ def record_multiple(duration, filename=None):
     print("Looking for streams")
     # Gets all LSL streams within the system
     streams = resolve_streams()
+    print(len(streams))
     if len(streams) < 3:
         raise ValueError("Insufficient Streams")
     # Assign each used stream to an inlet
@@ -22,7 +23,7 @@ def record_multiple(duration, filename=None):
     # Get info and description of channels names for data dumping
     # Info for PPG
     info_eeg = inlet_eeg.info()
-    description_eeg = info_eeg.description()
+    description_eeg = info_eeg.desc()
     nchan_eeg = info_eeg.channel_count()
     ch_eeg = description_eeg.child('channels').first_child()
     ch_names_eeg = [ch_eeg.child_value('label')]
@@ -32,7 +33,7 @@ def record_multiple(duration, filename=None):
     
     # Info for PPG
     info_ppg = inlet_ppg.info()
-    description_ppg = info_ppg.description()
+    description_ppg = info_ppg.desc()
     nchan_ppg = info_ppg.channel_count()
     ch_ppg = description_ppg.child('channels').first_child()
     ch_names_ppg = [ch_ppg.child_value('label')]
@@ -52,9 +53,9 @@ def record_multiple(duration, filename=None):
 
     while True:
         try:
-            chunk_eeg, timestamp_eeg = inlet_eeg.pull_chunk(time_out=1.0)
-            chunk_ppg, timestamp_ppg = inlet_ppg.pull_chunk(time_out=1.0)
-            chung_marker, timestamp_markers = inlet_markers.pull_chunk(time_out=1.0)
+            chunk_eeg, timestamp_eeg = inlet_eeg.pull_chunk(timeout=1.0)
+            chunk_ppg, timestamp_ppg = inlet_ppg.pull_chunk(timeout=1.0)
+            chunk_marker, timestamp_markers = inlet_markers.pull_sample(timeout=1.0)
             if timestamp_markers and timestamp_eeg and timestamp_ppg:
                 timestamps1 = np.array(timestamp_eeg) - timestamp_markers
                 timestamps2 = np.array(timestamp_ppg) - timestamp_markers
@@ -67,4 +68,10 @@ def record_multiple(duration, filename=None):
     #                     timestamp_eeg, chunk_eeg, timestamp_ppg, chunk_ppg))
                 print("Difference betwwen EEG and markers {}".format(timestamps1))
                 print("Difference betwwen PPG and markers {}".format(timestamps2))
-                print('\n\n\n\nNEXT_CHUNK\n\n\n
+                print('\n\n\n\nNEXT_CHUNK\n\n\n\n')
+        except KeyboardInterrupt:
+            break
+
+
+if __name__ == "__main__":
+    record_multiple(5)
